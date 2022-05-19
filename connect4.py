98d6cfd7c33ab54b2be063c15644a6045a4a95d9
emiksic <95447053+emiksic@users.noreply.github.com>
2022-05-19 08:30:20 +0200

    Add files via upload

--- connect4.py
+++ connect4.py
@@ -1,15 +1,24 @@
 import pygame
 pygame.init()
+size = [1100, 1000]
+screen = pygame.display.set_mode(size)
+bg_img = pygame.image.load('bg.png')
 
+mat = []
+for i in range(6):
+    aaa = []
+    for j in range(7):
+        aaa.append(0)
+    mat.append(aaa)
 
-screen = pygame.display.set_mode((400,550))
-background = pygame.image.load('connect-four-bg.jpg')
+print(mat)
 
 running = True
 while running:
+    pygame.display.flip()
+    screen.blit(bg_img, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-
-    screen.fill((255, 0, 0))
-    pygame.display.update()
+            running = False
+pygame.quit()
+    
